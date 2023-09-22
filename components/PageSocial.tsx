d2ef3af3e49a9729b763c041b4d689c18ee5629c
nealyguo <nealyguo@tencent.com>
2023-09-22 21:37:23 +0800

    add tiktok

--- components/PageSocial.tsx
+++ components/PageSocial.tsx
@@ -41,7 +41,7 @@ const socialLinks: SocialLink[] = [
     href: `https://github.com/`,
     title: `GitHub @${config.github}`,
     icon: (
-      <svg xmlns="http://www.w3.org/2000/svg"  viewBox='0 0 24 24'> 
+      <svg xmlns="http://www.w3.org/2000/svg"  viewBox='0 0 240 240'> 
         <path d="M9 0h1.98c.144.715.54 1.617 1.235 2.512C12.895 3.389 13.797 4 15 4v2c-1.753 0-3.07-.814-4-1.829V11a5 5 0 1 1-5-5v2a3 3 0 1 0 3 3V0Z"/> 
       </svg>
     )
@@ -52,7 +52,7 @@ const socialLinks: SocialLink[] = [
     href: `https://www.linkedin.com/in/${config.linkedin}`,
     title: `LinkedIn ${config.author}`,
     icon: (
-      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 24 24'>
+      <svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 240 240'>
         <path d='M6.5 21.5h-5v-13h5v13zM4 6.5C2.5 6.5 1.5 5.3 1.5 4s1-2.4 2.5-2.4c1.6 0 2.5 1 2.6 2.5 0 1.4-1 2.5-2.6 2.5zm11.5 6c-1 0-2 1-2 2v7h-5v-13h5V10s1.6-1.5 4-1.5c3 0 5 2.2 5 6.3v6.7h-5v-7c0-1-1-2-2-2z' />
       </svg>
     )
