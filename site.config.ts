11c8689be40effb4b10ad0512647eb6f1b4860f0
nealyguo <nealyguo@tencent.com>
2023-09-22 10:19:06 +0800

    add pageid

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '7875426197cf461698809def95960ebf',
+  rootNotionPageId: '516a813a7e3c41198d0ff02e233051a6',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
